e4741a337e60abbebe8a43fcff854a10d290919c
Saimulhoque <saimulhoque8214@gmail.com>
2020-11-17 13:24:05 +0600

    update

---
+++ app/src/main/java/com/forbit/movizo/IndianMoviesActivity.java
@@ -0,0 +1,14 @@
+package com.forbit.movizo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndianMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indian_movies);
+    }
+}
